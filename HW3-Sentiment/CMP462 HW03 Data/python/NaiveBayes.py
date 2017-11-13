3f150e300f690ebe2b668390eede20da79867a38
unknown <nguyenthicamvan711@gmail.com>
2017-11-13 16:36:42 +0700

    HW03 Sentiment Done

--- HW3-Sentiment/CMP462 HW03 Data/python/NaiveBayes.py
+++ HW3-Sentiment/CMP462 HW03 Data/python/NaiveBayes.py
@@ -6,7 +6,7 @@
 # The area for you to implement is marked with TODO!
 # Generally, you should not need to touch things *not* marked TODO
 #
-# Remember that when you submit your code, it is not run from the command line 
+# Remember that when you submit your code, it is not run from the command line
 # and your main() will *not* be run. To be safest, restrict your changes to
 # addExample() and classify() and anything you further invoke from there.
 #
@@ -21,7 +21,7 @@ import string
 
 class NaiveBayes:
   class TrainSplit:
-    """Represents a set of training/testing data. self.train is a list of Examples, as is self.test. 
+    """Represents a set of training/testing data. self.train is a list of Examples, as is self.test.
     """
     def __init__(self):
       self.train = []
@@ -41,73 +41,81 @@ class NaiveBayes:
     self.FILTER_STOP_WORDS = False
     self.stopList = set(self.readFile('../data/english.stop'))
     self.numFolds = 10
-    
     # count the number of each type of classes
-    self.classes = collections.defaultdict(lambda: 0)
-    # count(w_i, c_j)
-    self.nbLikehood = collections.defaultdict(lambda: 0)
-    # count the total number of all classes
+    self.classes = collections.defaultdict(lambda:0)
+  # count (w_i,c_j)
+    self.nbLikelihood = collections.defaultdict(lambda:0)
+  # count the total number of all classes
     self.totalClassNum = 0
-    # count the vocabulary of all words in all the classes
-    self.vocabulary = collections.defaultdict(lambda: 0)
-    # count the total number of words in each class
-    self.total_count_per_class = collections.defaultdict(lambda: 0)
+  # count the vocab of all words in all the classes
+    self.vocabulary = collections.defaultdict(lambda:0)
+  # count the total number of words in each class
+    self.total_count_per_class = collections.defaultdict(lambda:0)
   #############################################################################
-  # TODO TODO TODO TODO TODO 
-  
+  # TODO TODO TODO TODO TODO
+
+
   def classify(self, words):
     """ TODO
       'words' is a list of words to classify. Return 'pos' or 'neg' classification.
     """
-    classType = ''
-    prob_prior_pos = float(self.classes['pos']) / float(self.totalClassNum)
-    prob_prior_neg = float(self.classes['neg']) / float(self.totalClassNum)
+    classType =''
+    prob_prior_pos = float(self.classes['pos'])/float(self.totalClassNum)
+    prob_prior_neg = float(self.classes['neg'])/float(self.totalClassNum)
     prob_pos = 0
     prob_neg = 0
     for word in words:
-        type = 'pos'
-        count_word_class = self.nbLikehood[(word, type)]
-        count_totalword_class = self.total_count_per_class[type]
-        prob_pos += math.log((count_word_class + 1.0) / (count_totalword_class + len(self.vocabulary)))
-        
-        type = 'neg'
-        count_word_class = self.nbLikehood[(word, type)]
-        count_totalword_class = self.total_count_per_class[type]
-        prob_neg += math.log((count_word_class + 1.0) / (count_totalword_class + len(self.vocabulary)))
+      type = 'pos'
+      count_word_class  = self.nbLikelihood[(word, type)]
+      count_total_word_class = self.total_count_per_class[type]
+      prob_pos += math.log((count_word_class + 1.0)/(count_total_word_class + len(self.vocabulary)))
+
+      type = 'neg'
+      count_word_class = self.nbLikelihood[(word, type)]
+      count_total_word_class = self.total_count_per_class[type]
+      prob_pos += math.log((count_word_class + 1.0)/(count_total_word_class + len(self.vocabulary)))
+
     prob_pos += math.log(prob_prior_pos)
     prob_neg += math.log(prob_prior_neg)
-    
-    if prob_pos - prob_neg > 0:
-        classType = 'pos'
+
+    if(prob_pos - prob_neg) > 0:
+      classType = 'pos'
     else:
-        classType = 'neg'
-        
+      classType = 'neg'
+    
     return classType
-  
+
 
   def addExample(self, klass, words):
     """
      * TODO
      * Train your model on an example document with label klass ('pos' or 'neg') and
      * words, a list of strings.
-     * You should store whatever data structures you use for your classifier 
+     * You should store whatever data structures you use for your classifier
      * in the NaiveBayes class.
      * Returns nothing
     """
     self.classes[klass] += 1
     self.totalClassNum += 1
     for word in words:
-        self.vocabulary[word] += 0
-        self.nbLikehood[(word, klass)] += 1
-        self.total_count_per_class[klass] += 1
-    
-  # TODO TODO TODO TODO TODO 
+      self.vocabulary[word] += 0
+      self.nbLikelihood[(word, klass)] += 1
+      self.total_count_per_class[klass] += 1
+
+  def filterStopWords(self, words):
+    """
+    * TODO
+    * Filters stop words found in self.stopList.
+    """
+    return words
+
+  # TODO TODO TODO TODO TODO
   #############################################################################
-  
-  
+
+
   def readFile(self, fileName):
     """
-     * Code for reading a file.  you probably don't want to modify anything here, 
+     * Code for reading a file.  you probably don't want to modify anything here,
      * unless you don't like the way we segment files.
     """
     contents = []
@@ -115,17 +123,17 @@ class NaiveBayes:
     for line in f:
       contents.append(line)
     f.close()
-    result = self.segmentWords('\n'.join(contents)) 
+    result = self.segmentWords('\n'.join(contents))
     return result
 
-  
+
   def segmentWords(self, s):
     """
      * Splits lines on whitespace for file reading
     """
     return s.split()
 
-  
+
   def trainSplit(self, trainDir):
     """Takes in a trainDir, returns one TrainSplit with train set."""
     split = self.TrainSplit()
@@ -152,7 +160,7 @@ class NaiveBayes:
 
   def crossValidationSplits(self, trainDir):
     """Returns a lsit of TrainSplits corresponding to the cross validation splits."""
-    splits = [] 
+    splits = []
     posTrainFileNames = os.listdir('%s/pos/' % trainDir)
     negTrainFileNames = os.listdir('%s/neg/' % trainDir)
     #for fileName in trainFileNames:
@@ -188,14 +196,14 @@ class NaiveBayes:
       guess = self.classify(words)
       labels.append(guess)
     return labels
-  
+
   def buildSplits(self, args):
     """Builds the splits for training/testing"""
-    trainData = [] 
+    trainData = []
     testData = []
     splits = []
     trainDir = args[0]
-    if len(args) == 1: 
+    if len(args) == 1:
       print ('[INFO]\tPerforming %d-fold cross-validation on data set:\t%s' % (self.numFolds, trainDir))
 
       posTrainFileNames = os.listdir('%s/pos/' % trainDir)
@@ -240,34 +248,36 @@ class NaiveBayes:
       negTestFileNames = os.listdir('%s/neg/' % testDir)
       for fileName in posTestFileNames:
         example = self.Example()
-        example.words = self.readFile('%s/pos/%s' % (testDir, fileName)) 
+        example.words = self.readFile('%s/pos/%s' % (testDir, fileName))
         example.klass = 'pos'
         split.test.append(example)
       for fileName in negTestFileNames:
         example = self.Example()
-        example.words = self.readFile('%s/neg/%s' % (testDir, fileName)) 
+        example.words = self.readFile('%s/neg/%s' % (testDir, fileName))
         example.klass = 'neg'
         split.test.append(example)
       splits.append(split)
     return splits
-  
-  def filterStopWords(self, words):
-    """Filters stop words."""
-    filtered = []
-    for word in words:
-      if not word in self.stopList and word.strip() != '':
-        filtered.append(word)
-    return filtered
-
-
 
 def main():
   nb = NaiveBayes()
-  #(options, args) = getopt.getopt(sys.argv[1:], 'f')
+  # (options, args) = getopt.getopt(sys.argv[1:], 'f')
+
+  # default parameters: no stop word filtering, and
+  # training/testing on ../data/imdb1
+  # if len(sys.argv) < 2:
+  #     options = [('','')]
+  #     args = ['../data/imdb1/']
+  # else:
+  #     (options, args) = getopt.getopt(sys.argv[1:], 'f')
+  # if ('-f','') in options:
+  #   nb.FILTER_STOP_WORDS = True
+
+  
   (options, args) = [('-f', '')], ['../data/imdb1'] 
   if ('-f','') in options:
     nb.FILTER_STOP_WORDS = True
-  
+
   splits = nb.buildSplits(args)
   avgAccuracy = 0.0
   fold = 0
@@ -279,7 +289,7 @@ def main():
       if nb.FILTER_STOP_WORDS:
         words =  classifier.filterStopWords(words)
       classifier.addExample(example.klass, words)
-  
+
     for example in split.test:
       words = example.words
       if nb.FILTER_STOP_WORDS:
@@ -290,7 +300,7 @@ def main():
 
     accuracy = accuracy / len(split.test)
     avgAccuracy += accuracy
-    print ('[INFO]\tFold %d Accuracy: %f' % (fold, accuracy) )
+    print ('[INFO]\tFold %d Accuracy: %f' % (fold, accuracy))
     fold += 1
   avgAccuracy = avgAccuracy / fold
   print ('[INFO]\tAccuracy: %f' % avgAccuracy)
